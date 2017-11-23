b827c092fec701fa31eebf71a194bcb140641a2c
axetroy <troy450409405@gmail.com>
2017-11-23 15:35:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+## md5 cli tool
+
+[![Build Status](https://travis-ci.org/axetroy/md5.svg?branch=master)](https://travis-ci.org/axetroy/md5)
+![License](https://img.shields.io/badge/license-Apache-green.svg)
+
+Display the file/dir md5 value
+
+## Usage
+
+```bash
+go install github.com/axetroy/md5
+./$GOPATH/bin/md5
+
+# output the result like this
+
+63221d5cb2a8851fb0369c986171b493  .all-contributorsrc
+55e91fcd2c32356ba54d9204d03cf581  .editorconfig
+da8aa4de3a27334fd8be2c2fde195de3  .gitattributes
+df10ad3a47f0077b52924b69e2a5b20a  .gitignore
+22aaefc1eb93b86248308cd266fa8c66  .gpmrc
+af03c276ef40718614dc275ec14c5fa1  .travis.yml
+7cbce181f3c83967407f0579a475a53a  LICENSE
+af21837b0251ff6e08ddeaf4adbac7a3  README.md
+bbe5505e463e8af8ffb2e97a731e4e4c  build
+daa537d2c4de1efcd0932078dab3f13f  contributing.md
+5679eaa2f73385cc25d5512d4eed2995  main.go
+```
+
+Print the cli help information
+
+```bash
+
+md5 --help
+
+NAME:
+   md5 - md5 [path]
+
+USAGE:
+   main [global options] command [command options] [arguments...]
+
+VERSION:
+   0.0.0
+
+DESCRIPTION:
+   Display the file/dir md5 value
+
+COMMANDS:
+     help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h     show help
+   --version, -v  print the version
+```
+
+## Contributing
+
+[Contributing Guid](https://github.com/axetroy/md5/blob/master/CONTRIBUTING.md)
+
+## Contributors
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+| [<img src="https://avatars1.githubusercontent.com/u/9758711?v=3" width="100px;"/><br /><sub>Axetroy</sub>](http://axetroy.github.io)<br />[💻](https://github.com/axetroy/md5/commits?author=axetroy) [🐛](https://github.com/axetroy/md5/issues?q=author%3Aaxetroy) 🎨 |
+| :---: |
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## License
+
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Faxetroy%2Fmd5.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2Faxetroy%2Fmd5?ref=badge_large)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+  "os"
+  "path"
+  "io/ioutil"
+  "crypto/md5"
+  "io"
+  "math"
+  "fmt"
+  "encoding/hex"
+  "github.com/urfave/cli"
+)
+
+const (
+  fileChunk = 8192 // 8KB
+)
+
+/**
+Calculate md5 of a file
+ */
+func calcMd5(filePath string) (string) {
+  file, err := os.Open(filePath)
+
+  if err != nil {
+    panic(err.Error())
+  }
+
+  defer file.Close()
+
+  // calculate the file size
+  info, _ := file.Stat()
+
+  fileSize := info.Size()
+
+  blocks := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+  hash := md5.New()
+
+  for i := uint64(0); i < blocks; i++ {
+    blockSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+    buf := make([]byte, blockSize)
+
+    file.Read(buf)
+    io.WriteString(hash, string(buf)) // append into the hash
+  }
+
+  md5string := hex.EncodeToString(hash.Sum(nil))
+
+  return md5string
+}
+
+func main() {
+  app := cli.NewApp()
+
+  app.Name = "md5"
+  app.Usage = "md5 [path]"
+  app.Description = "Display the file/dir md5 value"
+
+  app.Action = func(c *cli.Context) (err error) {
+
+    var (
+      cwd      string
+      pathStat os.FileInfo
+      relative = c.Args().Get(0)
+      distPath string // 最终目录/文件
+    )
+
+    if path.IsAbs(relative) {
+      distPath = relative
+    } else {
+      if cwd, err = os.Getwd(); err != nil {
+        return
+      } else {
+        distPath = path.Join(cwd, distPath)
+      }
+    }
+
+    pathStat, err = os.Stat(distPath)
+
+    if err != nil {
+      panic(err)
+      return
+    }
+
+    // if is a dir
+    if pathStat.IsDir() {
+      if fileInfos, err := ioutil.ReadDir(distPath); err != nil {
+        return err
+      } else {
+        for i := range fileInfos {
+          info := fileInfos[i]
+
+          if info.IsDir() {
+            // skip if it is a dir
+            continue
+          } else {
+            fileName := info.Name()
+            absFilePath := path.Join(distPath, fileName)
+            md5string := calcMd5(absFilePath)
+            fmt.Println(md5string + "  " + fileName)
+          }
+        }
+      }
+    }
+
+    return nil
+  }
+
+  app.Run(os.Args)
+}
